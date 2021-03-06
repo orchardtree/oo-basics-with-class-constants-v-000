858fd64b76b5bc1e47e13e5db3fc19aa14eabf30
Jesse Wenger <wengerj@gmail.com>
2021-03-06 19:13:06 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,21 @@
 class Shoe
+  BRANDS = []
+  
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
   def initialize(brand)
     @brand = brand
+    if BRANDS.none?{|existing_brand| existing_brand == brand}
+      BRANDS << brand
+    end
+  end
+  
+  def brand=(brand)
+    @brand = brand
+    if BRANDS.none?{|existing_brand| existing_brand == brand}
+      BRANDS << brand
+    end
   end
 
   def cobble
