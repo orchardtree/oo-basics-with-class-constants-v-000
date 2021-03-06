2d6028efef84e4335f5a6540b8b9460e5e6bf65a
Jesse Wenger <wengerj@gmail.com>
2021-03-06 18:59:11 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -4,8 +4,10 @@ class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
 
-  def initialize(title)
+  def initialize(title, genre)
     @title = title
+    @genre = genre
+    GENRES << genre
   end
 
   def turn_page
